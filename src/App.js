e70a3fe21bfe1231cf157d2b078ef49636c6a0e2
Kyle Coberly <kyle.coberly@gmail.com>
2023-07-24 15:11:20 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [students, setStudents] = useState([])
+	const [name, setName] = useState("")
+
+	useEffect(() => {
+		fetch("http://localhost:4000/students")
+			.then(response => response.json())
+			.then(response => {
+				setStudents(response.students)
+			})
+	}, [])
+
+	const updateName = (event) => {
+		setName(event.target.value)
+	}
+
+	const addName = (event) => {
+		event.preventDefault()
+		fetch("http://localhost:4000/students", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ student: { name: event.target["name"].value } })
+		}).then(response => response.json())
+			.then(response => {
+				setStudents([...students, response.student])
+				setName("")
+			})
+	}
+
+	return (
+		<div className="App">
+			<h1>Students</h1>
+			<form onSubmit={addName}>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					className="name"
+					value={name}
+					onInput={updateName}
+				/>
+
+				<input type="submit" value="Add name" />
+			</form>
+			<ul>
+				{students.map(({ id, name }) => (
+					<li key={id}>{name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
